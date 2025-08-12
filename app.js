b50f914196c4a5ebf9c881209396d6da7c0cca76
9tw <gerdo.tewel@gmail.com>
2025-08-12 20:31:26 +0900

    fix app

--- app.js
+++ app.js
@@ -9,5 +9,5 @@ app.use(express.json());
 app.use("/", routes);
 
 app.listen(port, () => {
-  console.log(`Server up and running on port ${port}`);
+  console.log(`Server up and run on port ${port}`);
 });
