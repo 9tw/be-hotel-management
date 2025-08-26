ba412f3d42ac466d3ad9289f8f07b0f8c86845af
9tw <gerdo.tewel@gmail.com>
2025-08-26 17:17:31 +0900

    change for deploy devs

--- config/config.js
+++ config/config.js
@@ -1,8 +1,15 @@
 require("dotenv").config();
+const process = require("process");
 
 module.exports = {
   development: {
-    url: process.env.DB_URL,
-    dialect: process.env.DB_DIALECT,
+    use_env_variable: process.env.DB_URL,
+    dialect: process.env.DB_DIALEC,
+    dialectOptions: {
+      ssl: {
+        require: true,
+        rejectUnauthorized: false,
+      },
+    },
   },
 };
