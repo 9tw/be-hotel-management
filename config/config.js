ad9cb6ed43126bffdc362134f4e738bd793008ef
9tw <gerdo.tewel@gmail.com>
2025-08-26 16:52:18 +0900

    change for deploy devs

--- config/config.js
+++ config/config.js
@@ -2,10 +2,7 @@ require("dotenv").config();
 
 module.exports = {
   development: {
-    username: process.env.DB_USERNAME,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME,
-    host: process.env.DB_HOST,
+    url: process.env.DB_URL,
     dialect: process.env.DB_DIALECT,
   },
 };
