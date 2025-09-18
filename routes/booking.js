962f41e5a15fe760c8abf9c235db20816b17c9f9
9tw <gerdo.tewel@gmail.com>
2025-09-18 18:18:32 +0900

    add tomorrow check out on dashboard

--- routes/booking.js
+++ routes/booking.js
@@ -6,17 +6,19 @@ const {
   update,
   destroy,
   getBookingById,
-  getCheckOutToday,
   getCheckInToday,
   getCheckInTomorrow,
+  getCheckOutToday,
+  getCheckOutTomorrow,
   getBookingToday,
 } = require("../controllers/bookingController");
 
 router.get("/", index);
 router.get("/get/:id", getBookingById);
-router.get("/check-out-today", getCheckOutToday);
 router.get("/check-in-today", getCheckInToday);
 router.get("/check-in-tomorrow", getCheckInTomorrow);
+router.get("/check-out-today", getCheckOutToday);
+router.get("/check-out-tomorrow", getCheckOutTomorrow);
 router.get("/today", getBookingToday);
 router.post("/", create);
 router.put("/:id", update);
